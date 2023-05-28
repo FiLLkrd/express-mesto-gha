129f14f3aed788dc194a2da5028317a3ca825795
FiLLkrd <ma.antonoff2012@yandex.ru>
2023-05-28 17:34:23 +0300

    загрузка проекта

---
+++ controllers/users.js
@@ -0,0 +1,126 @@
+const usersModel = require('../models/users');
+
+const getUsers = (req, res) => {
+  usersModel.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'На сервере произошла ошибка',
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+};
+
+const getUserById = (req, res) => {
+  usersModel
+    .findById(req.params.userId)
+    .orFail()
+    .then((users) => {
+      res.send({ users });
+    })
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({
+          message: 'Пользователь с указанным _id не найден',
+        });
+      }
+      if (err.name === 'CastError') {
+        return res.status(400).send({
+          message: 'Переданы некорректные данные',
+          err: err.message,
+          stack: err.stack,
+        });
+      }
+      return res.status(500).send({
+        message: 'На сервере произошла ошибка',
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+};
+
+const createUser = (req, res) => {
+  usersModel
+    .create(req.body)
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'На сервере произошла ошибка',
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+};
+
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  usersModel
+    .findByIdAndUpdate(
+      req.user._id,
+      { name, about },
+      {
+        new: true,
+        runValidators: true,
+      },
+    )
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({
+          message: 'переданы некорректные данные в методы обновления пользователя или профиля',
+          err: err.message,
+          stack: err.stack,
+        });
+      }
+      return res.status(500).send({
+        message: 'На сервере произошла ошибка',
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  usersModel
+    .findByIdAndUpdate(
+      req.user._id,
+      { avatar },
+      {
+        new: true,
+        runValidators: true,
+      },
+    )
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({
+          message: 'переданы некорректные данные в методы обновления аватара',
+          err: err.message,
+          stack: err.stack,
+        });
+      }
+      return res.status(500).send({
+        message: 'На сервере произошла ошибка',
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  updateAvatar,
+};
