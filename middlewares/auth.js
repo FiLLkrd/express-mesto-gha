91cfe60e4a99e4614529b4e5ac05b2a2b3cd9f7f
FiLLkrd <ma.antonoff2012@yandex.ru>
2023-06-11 16:36:05 +0300

    устранение ошибок в автотестах_2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.starWith('Bearer ')) {
-    return res.status(BAD_REQUEST).send({ message: 'Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
 
   const token = authorization.replace('Bearer ', '');
